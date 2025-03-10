2df5808acae6783b771803cf44f3c56078f82a64
Cyril BERAULT <berault.cyril@gmail.com>
2025-03-10 16:00:31 +0100

    les fiches fonctionnent, je commence le formulaire

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 let idFiche = 0;
 function newFiche() {
+  const sectionFiches = document.querySelector(".fiches");
   const newFiche = document.createElement("article");
+
   newFiche.classList.add("fiche");
   newFiche.id = `${idFiche}`;
+  newFiche.setAttribute("onclick", "toggleFiche(this)");
 
-  const sectionFiches = document.querySelector(".fiches");
-
+  const contenuFiche = document.createElement("div");
+  contenuFiche.classList.add("contenu-fiche");
   const titreFiche = document.createElement("h3");
   titreFiche.innerText = ` ${idFiche} titre de la nouvelle fiche`; // nom de la fiche
+  const detail = document.createElement("p");
+  detail.classList.add("detail");
+  detail.innerText = "je suis un détail";
+  sectionFiches.appendChild(newFiche); // ajout de la fiche
+
+  // bouton supprimer
   const boutonSupprimer = document.createElement("button");
   boutonSupprimer.setAttribute("onclick", "supprimerFiche(this)"); // bouton supprimer
   boutonSupprimer.innerText = "X";
-  sectionFiches.appendChild(newFiche); // ajout de la fiche
+  boutonSupprimer.id = "boutonSupprimer";
 
-  newFiche.appendChild(titreFiche);
+  // insertion des elements
+  contenuFiche.appendChild(titreFiche);
+  contenuFiche.appendChild(detail);
+  newFiche.appendChild(contenuFiche);
   newFiche.appendChild(boutonSupprimer);
 
   console.log(newFiche.id);
@@ -27,9 +39,12 @@ function supprimerFiche(bouton) {
 function formulaireNouvelleFiche() {
   const modal = document.getElementById("modal");
   modal.showModal();
-  modal.classList.add("modal")
+
   document
     .getElementById("closeModal")
     .addEventListener("click", () => modal.close());
-    
+}
+
+function toggleFiche(element) {
+  element.classList.toggle("ouverte"); // Ajoute ou enlève la classe "ouverte"
 }
