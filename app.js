8ae795935f097074b1677c741d8f99b04fa4779f
Cyril BERAULT <berault.cyril@gmail.com>
2025-03-12 16:29:25 +0100

    ajout du btn modifier

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-
-
-
-
 let idFiche = 0;
-let voyage = []
-
+let voyage = [];
 
 function newFiche() {
-  
   // récuperation des data du formulaire
-  const dataFiche = recupDataFormulaire()
-  
+  const dataFiche = recupDataFormulaire();
+
   // création de la fiche
   const sectionFiches = document.querySelector(".fiches");
   const newFiche = document.createElement("article");
@@ -20,44 +14,59 @@ function newFiche() {
 
   const contenuFiche = document.createElement("div");
   contenuFiche.classList.add("contenu-fiche");
+
+  // Titre de la fiche
   const titreFiche = document.createElement("h3");
   titreFiche.innerText = ` ${idFiche} ${dataFiche.titreFiche}`; // nom de la fiche
+
+  // Detail de la fiche
   const detail = document.createElement("p");
-  detail.classList.add("detail");
+  detail.classList.add("detail" );
   detail.innerText = `${dataFiche.detail}`;
   sectionFiches.appendChild(newFiche); // ajout de la fiche
+
+  // Durée de la fiche
+  const duree = document.createElement("p");
+  duree.classList.add("duree", "detail");
+  duree.innerText = `la durée est de ${dataFiche.duree} H`;
 
   // bouton supprimer
   const boutonSupprimer = document.createElement("button");
   boutonSupprimer.setAttribute("onclick", "supprimerFiche(this)"); // bouton supprimer
   boutonSupprimer.innerText = "X";
-  boutonSupprimer.id = "boutonSupprimer";
-
+  boutonSupprimer.classList.add("bouton-cache")
+  
+  
+  // bouton modifier
+  const boutonModifier = document.createElement("button");
+  boutonModifier.setAttribute("onclick", "supprimerFiche(this)"); // bouton supprimer
+  boutonModifier.innerText = "...";
+  boutonModifier.classList.add("bouton-cache")
+  boutonModifier.id = "btn-modifier"
 
   // insertion des elements
   contenuFiche.appendChild(titreFiche);
   contenuFiche.appendChild(detail);
+  contenuFiche.appendChild(duree);
   newFiche.appendChild(contenuFiche);
+  newFiche.appendChild(boutonModifier);
   newFiche.appendChild(boutonSupprimer);
 
- 
-
   idFiche++;
-  ajouterFiche(dataFiche)
+  ajouterFiche(dataFiche);
 }
 
 // function d'ajout de la fiche au tableau voyage
 function ajouterFiche(nouvelleFiche) {
-  voyage.push(nouvelleFiche)
-  console.log(voyage)
-
+  voyage.push(nouvelleFiche);
+  console.log(voyage);
 }
 
 // fonction de suppression de la fiche par le bouton et aussi du tableau voyage
 function supprimerFiche(bouton) {
   bouton.parentNode.remove();
-  voyage.splice(bouton.parentNode, 1)
-  console.log(voyage)
+  voyage.splice(bouton.parentNode, 1);
+  console.log(voyage);
 }
 
 function formulaireNouvelleFiche() {
@@ -72,13 +81,12 @@ function formulaireNouvelleFiche() {
 function toggleFiche(element) {
   element.classList.toggle("ouverte"); // Ajoute ou enlève la classe "ouverte"
 }
-function recupDataFormulaire () {
-  const titreFiche = document.getElementById("titre-fiche").value
-  const detail = document.getElementById("detail").value
-  const typeFiche = document.getElementById("type-de-fiche").value
-  const duree = document.getElementById("duree").value
-  const dataFiche = {typeFiche, titreFiche, detail, duree}
-  console.log(dataFiche)
-  return dataFiche
+function recupDataFormulaire() {
+  const titreFiche = document.getElementById("titre-fiche").value;
+  const detail = document.getElementById("detail").value;
+  const typeFiche = document.getElementById("type-de-fiche").value;
+  const duree = document.getElementById("duree").value;
+  const dataFiche = { typeFiche, titreFiche, detail, duree };
+  console.log(dataFiche);
+  return dataFiche;
 }
-
